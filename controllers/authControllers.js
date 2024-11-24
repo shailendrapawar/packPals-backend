cfffaa05c0cf8995b6e53607a6e93bab070600fb
shailendrapawar <shailendrapawar980@gmail.com>
2024-11-24 14:12:22 +0530

    ADDED: password change functionality

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -1,75 +1,137 @@
 const AuthModel = require("../models/authmodel.js");
-const bcrypt=require("bcrypt")
+const bcrypt = require("bcrypt")
 
 class AuthController {
+
+    // for login =======================================
     static login = async (req, res) => {
-
-        const {email,password}=req.body;
-
+        const { email, password } = req.body;
         const isUser = await AuthModel.findOne({ email: email })
-        if(isUser){
-            const isValid=await bcrypt.compareSync(password,isUser.password);
-            if(isValid){
+        if (isUser) {
+            const isValid = await bcrypt.compareSync(password, isUser.password);
+            if (isValid) {
                 res.json({
-                    msg:"authentic User",
-                    status:200,
-                    id:isUser._id
+                    msg: "authentic User",
+                    status: 200,
+                    id: isUser._id
                 })
-            }else{
+            } else {
                 res.json({
-                    msg:"incorrect password",
-                    status:400,
+                    msg: "incorrect password",
+                    status: 400,
                 })
             }
-        
-        }else{
+
+        } else {
             res.json({
-                msg:"user not registered",
-                status:400
+                msg: "user not registered",
+                status: 400
             })
         }
 
-
     }
 
+
+    // for registeration================================
     static register = async (req, res) => {
         const { name, email, number, password, gender, dob } = req.body;
 
         const isUser = await AuthModel.findOne({ email: email })
         if (!isUser) {
-
-            const salt=await bcrypt.genSalt(10)
-            const hashPass=await bcrypt.hashSync(password,salt)
+            const salt = await bcrypt.genSalt(10)
+            const hashPass = await bcrypt.hashSync(password, salt)
             console.log(hashPass)
 
-            const newUser=new AuthModel({
-                userName:name,
-                email:email,
-                number:number,
-                password:hashPass,
-                gender:gender,
+            const newUser = new AuthModel({
+                userName: name,
+                email: email,
+                number: number,
+                password: hashPass,
+                gender: gender,
+                dob: dob
             })
-
-            const isCreated=await newUser.save()
+            const isCreated = await newUser.save()
             console.log(isCreated)
             res.json({
-                msg:"user created",
-                status:200
+                msg: "user created",
+                status: 200
             })
-        }else{
+        } else {
             res.json({
-                msg:"user already exists",
-                status:400
+                msg: "user already exists",
+                status: 400
             })
         }
     }
 
 
-    static changePassword = async (req, res) => {
-       
 
-        
+    // for changing the password -========================================
+    static changePassword = async (req, res) => {
+
+        const { email, newPassword, oldPassword } = req.body;
+        const isUser = await AuthModel.findOne({ email: email })
+
+        if (isUser) {
+            const storedPassword = isUser.password;
+            const isMatch = await bcrypt.compare(oldPassword, storedPassword);
+
+            if (isMatch) {
+                const salt=await bcrypt.genSalt(10);
+                const newHashPass=await bcrypt.hashSync(newPassword,salt)
+
+                // saving in DB
+                const isSaved=await AuthModel.findOneAndUpdate({email:email},{
+                    $set:{password:newHashPass}
+                })
+
+                if(isSaved){
+                    res.json({
+                        msg:"password changed",
+                        status:200
+                    })
+                }else{
+                    res.json({
+                        msg:"password not changed",
+                        status:400
+                    })
+                }
+
+            } else {
+                res.json({
+                    msg: "wrong credentials",
+                    status: 400
+                })
+            }
+
+
+
+
+
+        }else{
+            res.json({
+                msg: "user dosent exist",
+                status: 400
+            })
+        }
+
+
+
+
+
+
+
+
     }
+
+
+
+
+
+
+
+
+
 
 
 }
